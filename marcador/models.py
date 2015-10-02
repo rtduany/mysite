8d10f7f19362ab19ff9ed9904d573b290e3c69a6
rtduany <rtduany@gmail.com>
2015-10-02 17:33:04 +0300

    added class Tag

--- marcador/models.py
+++ marcador/models.py
@@ -5,4 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
 
