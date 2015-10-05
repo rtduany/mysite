808a6e6691753fdde46f99b965bd8dddb04cb1b0
rtduany <rtduany@gmail.com>
2015-10-06 00:58:24 +0300

    added a couple of url code

--- marcador/urls.py
+++ marcador/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^user/(?P<username>[-\w]+)/$', 'marcador.views.bookmark_user',
-        name='marcador_bookmark_user'),
-    url(r'^$', 'marcador.views.bookmark_list', name='marcador_bookmark_list'),
+	url(r'^user/(?P<username>[-\w]+)/$', 'marcador.views.bookmark_user', 		name='marcador_bookmark_user'),
+	url(r'^$', 'marcador.views.bookmark_list', name='marcador_bookmark_list'),
 ]
