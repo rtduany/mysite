3bb292fbd3cf8693840bbcf6d855ecbdb206b616
rtduany <rtduany@gmail.com>
2015-10-05 19:38:23 +0300

    added a meta class to models.py file

--- marcador/models.py
+++ marcador/models.py
@@ -6,15 +6,19 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+	name = models.CharField(max_length=50, unique=True)
 
 class Bookmark(models.Model):
-    url = models.URLField()
-    title = models.CharField('title', max_length=255)
-    description = models.TextField('description', blank=True)
-    is_public = models.BooleanField('public', default=True)
+	url = models.URLField()
+	title = models.CharField('title', max_length=255)
+	description = models.TextField('description', blank=True)
+	is_public = models.BooleanField('public', default=True)
 	date_created = models.DateTimeField('date created')
-    date_updated = models.DateTimeField('date updated')
-    owner = models.ForeignKey(User, verbose_name='owner',
-        related_name='bookmarks')
-    tags = models.ManyToManyField(Tag, blank=True)
+	date_updated = models.DateTimeField('date updated')
+	owner = models.ForeignKey(User, verbose_name='owner', related_name='bookmarks')
+	tags = models.ManyToManyField(Tag, blank=True)
+
+	class Meta:
+		verbose_name = 'bookmark'
+		verbose_name_plural = 'bookmarks'
+		ordering = ['-date_created']
