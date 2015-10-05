fcb5ee1859802d5c14965cc59679089c52c14b82
rtduany <rtduany@gmail.com>
2015-10-06 00:28:01 +0300

    added views to views.py file

--- marcador/views.py
+++ marcador/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .models import Bookmark
 # Create your views here.
+
+
+def bookmark_list(request):
+	bookmarks = Bookmark.public.all()
+	context = {'bookmarks': bookmarks}
+	return render(request, 'marcador/bookmark_list.html', context)
