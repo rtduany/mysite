dd4805aa26ad51f8c73dd9798e129c6081f64082
rtduany <rtduany@gmail.com>
2015-10-08 00:00:55 +0300

    modified the global variable from tags to tag

--- marcador/templatetags/marcador_tags.py
+++ marcador/templatetags/marcador_tags.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.html import format_html_join
 
-from .models import Tag
+from ..models import Tag
 
 
 register = template.Library()
@@ -21,8 +21,8 @@ def tagcloud(context, owner=None):
 	if context['user'] == owner:
 		del filters['bookmark__is_public']
 
-	tags = Tag.objects.filter(**filters)
-	tags = tags.annotate(count=models.Count('bookmark'))
-	tags = tags.order_by('name').values_list('name', 'count')
+	tag = Tag.objects.filter(**filters)
+	tag = tag.annotate(count=models.Count('bookmark'))
+	tag = tag.order_by('name').values_list('name', 'count')
 	fmt = '<a href="%s?tag={0}">{0} ({1})</a>' % url
-	return format_html_join(', ', fmt, tags)
+	return format_html_join(', ', fmt, tag)
