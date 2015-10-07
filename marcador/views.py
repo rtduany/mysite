b1a15ad31ef166be70e708fe78650c10807e20e4
rtduany <rtduany@gmail.com>
2015-10-07 16:07:24 +0300

    added a second view that allows editing of bookmarks

--- marcador/views.py
+++ marcador/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BookmarkForm
@@ -24,16 +25,33 @@ def bookmark_user(request, username):
 
 @login_required
 def bookmark_create(request):
-    if request.method == 'POST':
-        form = BookmarkForm(data=request.POST)
-        if form.is_valid():
-            bookmark = form.save(commit=False)
-            bookmark.owner = request.user
-            bookmark.save()
-            form.save_m2m()
-            return redirect('marcador_bookmark_user',
-                username=request.user.username)
-    else:
-        form = BookmarkForm()
-    context = {'form': form, 'create': True}
-    return render(request, 'marcador/form.html', context)
+	if request.method == 'POST':
+		form = BookmarkForm(data=request.POST)
+		if form.is_valid():
+			bookmark = form.save(commit=False)
+			bookmark.owner = request.user
+			bookmark.save()
+			form.save_m2m()
+			return redirect('marcador_bookmark_user',
+							username=request.user.username)
+	else:
+		form = BookmarkForm()
+	context = {'form': form, 'create': True}
+	return render(request, 'marcador/form.html', context)
+
+
+@login_required
+def bookmark_edit(request, pk):
+	bookmark = get_object_or_404(Bookmark, pk=pk)
+	if bookmark.owner != request.user and not request.user.is_superuser:
+		raise PermissionDenied
+		if request.method == 'POST':
+			form = BookmarkForm(instance=bookmark, data=request.POST)
+			if form.is_valid():
+				form.save()
+				return redirect('marcador_bookmark_user',
+								username=request.user.username)
+	else:
+		form = BookmarkForm(instance=bookmark)
+	context = {'form': form, 'create': False}
+	return render(request, 'marcador/form.html', context)
