d0aec0e12c577353de3d279d2dc11fe325c0715a
rtduany <rtduany@gmail.com>
2015-10-07 15:43:14 +0300

    created a view that is used to create new bookmarks

--- marcador/views.py
+++ marcador/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import BookmarkForm
 from .models import Bookmark
 # Create your views here.
 
